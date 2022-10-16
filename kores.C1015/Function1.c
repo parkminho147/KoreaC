d27c2509896f1e290b76a6934e131368d656da83
chuminkyoung <chuu_u94@naver.com>
2022-10-16 12:49:47 +0900

    c언어20221016

---
+++ kores.C1015/Function1.c
@@ -0,0 +1,24 @@
+   #include <stdio.h>
+
+   //전역 변수 number
+int number = 0;
+
+void printNumber(void)
+{
+	printf("전역 변수number는%d을 저장하고 있다!\n", number);
+}
+
+void increasiNumber()
+{
+static int numb
+}
+
+int main(void)
+{
+	//지역 변수numbir
+	int number = 3;
+	printf("지역 변수number는%d을 저장하고 있다!\n", number);
+	printNumber();
+
+	return 0;
+}
